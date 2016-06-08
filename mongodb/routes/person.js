2ae1b973477ca8d0cc4fdbd0f43cf93ed464adfe
= <hello@imsean.me>
2016-06-07 23:56:13 -0500

    Make example for route better

--- mongodb/routes/person.js
+++ mongodb/routes/person.js
@@ -3,7 +3,24 @@ import Person from '../models/Person';
 exports.method = 'get';
 exports.path = '/person';
 exports.handler = function (req, res) {
-  res.status(200).send({
-    message: 'ok'
+  var name = req.param.name;
+  var age = req.param.age;
+  var address = req.param.address;
+
+  var person = new Person({
+    name,
+    age,
+    address
+  });
+  person.save(err => {
+    if (err) {
+      return res.status(400).send({
+        message: 'not ok'
+      });
+    }
+
+    res.status(200).send({
+      message: 'ok'
+    });
   });
 };
